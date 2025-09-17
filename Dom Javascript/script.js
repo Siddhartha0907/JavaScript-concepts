8e39bce3c99c00caf1095cc985b19dbed7985831
Siddhartha0907 <p.siddharth.9afc.12@gmail.com>
2025-09-17 22:53:35 +0530

    Add files via upload

---
+++ Dom Javascript/script.js
@@ -0,0 +1,8 @@
+alert("This is an Alert")
+const name = prompt("Enter your name")
+console.log(name)
+console.log(window)
+console.log(document)
+document.write("Hello " + name)
+document.title = "DOM"
+console.log(document.title)
